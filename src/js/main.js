d408858bb974b6fdcb64400c644fd189838d139c
Krzem <krzem5.dev@gmail.com>
2021-04-06 11:32:41 +0200

    Push Update 04/06/2021, 11:32:27

--- src/js/main.js
+++ src/js/main.js
@@ -68,11 +68,11 @@ function keydown(e){
 }
 function create_lines(){
 	fetch("./data/board.txt").then((r)=>r.text()).then(function(b){
-		b=b.split("\n")
-		w=b[0].split(" ")[0]
-		h=b[0].split(" ")[1]
-		CW=50
-		CH=50
+		let b=b.split("\n")
+		let w=b[0].split(" ")[0]
+		let h=b[0].split(" ")[1]
+		let CW=50
+		let CH=50
 		cnv.width=CW*w*2
 		cnv.height=CH*h
 		lines.push(new Line(0,0,cnv.width/2,0))
